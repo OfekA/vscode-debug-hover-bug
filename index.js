e2d7a60ced62dc07a0e10867c08cd736c0c9511e
Ofek Azulay <ofek.azulay@gmail.com>
2017-07-08 20:36:25 +0200

    Removed unecessary parts

--- index.js
+++ index.js
@@ -3,44 +3,45 @@ const async = require('async')
 let hook = {}
 hook.result = {}
 
+function timeout () {
+  return new Promise(function (resolve, reject) {
+      setTimeout(function() {
+        resolve()
+      }, 5000);
+    })
+}
+
 function runHook() {
   return hook => {
-    if (hook.data.info) {
-      let info = {
-        user: {
-          email: hook.params.user.email, 
-          id: hook.params.user.id
-        },
-        settings: {},
-        notifications: []
-      }
-      return new Promise(function (resolve, reject) {
-        async.parallel([
-          function(callback) {
-            hook.app.service('settings').find()
-              .then(settings => {
-                info.settings = settings[0]
-              })
-              .catch((err) => console.log('Error fetching settings for user'))
-              .then(() => callback(null, 'settings'))
-          },
-          function(callback) {
-            hook.app.service('notifications').find()
-              .then(notifications => {
-                info.notifications = notifications
-              })
-              .catch((err) => console.log('Error fetching notifications for user'))
-              .then(() => callback(null, 'notifications'))
-          }
-        ],
-        // optional callback
-        function(err, results) {
-            Object.assign(hook.result, info)
-            resolve()
-        })
-      })
+    let info = {
+      user: {},
+      settings: {},
+      notifications: []
     }
+    return new Promise(function (resolve, reject) {
+      async.parallel([
+        function(callback) {
+          timeout().then(() => {
+            info.settings = "settings"
+          })
+          .catch(() => console.log('err1'))
+          .then(() => callback(null, 'settings'))
+        },
+        function(callback) {
+          timeout().then(() => {
+            info.settings = "notifications"
+          })
+          .catch(() => console.log('err2'))
+          .then(() => callback(null, 'notifications'))
+        }
+      ],
+      // optional callback
+      function(err, results) {
+          Object.assign(hook.result, info)
+          resolve()
+      })
+    })
   }
 }
 
-runHook()
+runHook()(hook)
